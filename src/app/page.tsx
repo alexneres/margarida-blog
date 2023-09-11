19527801ec411981578be5277924ded0692faef1
Alex N Vieira <alexnvieira@protonmail.com>
2023-09-11 16:42:12 -0300

    feat: add sanity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,43 @@
-export default function Home() {
-  return <main></main>
+import { client } from '@/lib/sanity.client'
+import Link from 'next/link'
+import { TypedObject } from 'sanity'
+
+export type PostType = {
+  title: string
+  overview: string
+  content: TypedObject
+  _id: string
+  slug: {
+    current: string
+  }
+  _createdAt: string
+}
+
+async function getPosts() {
+  const query = '*[_type == "post"]'
+
+  const posts = await client.fetch(query)
+
+  return posts
+}
+
+export default async function Home() {
+  const posts = (await getPosts()) as PostType[]
+  return (
+    <main>
+      {posts.map((post) => (
+        <li key={post._id}>
+          <article>
+            <Link href={`/post/${post.slug.current}`}>
+              <div>
+                <h3>{post.title}</h3>
+              </div>
+
+              <p>{post.overview}</p>
+            </Link>
+          </article>
+        </li>
+      ))}
+    </main>
+  )
 }
---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import { PostType } from '@/app/page'
+import { urlFor } from '@/lib/sanity-image-url'
+import { client } from '@/lib/sanity.client'
+import { PortableText } from '@portabletext/react'
+import Image from 'next/image'
+
+async function getPost(slug: string) {
+  const query = `*[_type == "post" && slug.current == '${slug}'][0]`
+
+  const post = await client.fetch(query)
+
+  return post
+}
+
+export default async function Post({ params }: { params: { slug: string } }) {
+  const post = (await getPost(params.slug)) as PostType
+
+  const PortableTextComponent = {
+    types: {
+      image: ({ value }: { value: string }) => (
+        <Image src={urlFor(value).url()} alt="Image" />
+      ),
+    },
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <PortableText value={post.content} components={PortableTextComponent} />
+    </div>
+  )
+}
